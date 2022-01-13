c34c7c6ee620a110a41840c03ba96efc7a7ad8b2
chykh <chykh@mail.ru>
2022-01-13 16:46:18 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    Error() {
+    }
+
+    Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void show() {
+        System.out.println("Активная " + active + ". Статус " + status + ". Сообщение " + message);
+    }
+
+    public static void main(String[] args) {
+        Error pustaya = new Error();
+        pustaya.show();
+        Error pervaya = new Error(true, 1, "Ошибка воспроизыедения");
+        pervaya.show();
+        Error vtoraya = new Error(true, 2, "Ошибка записи");
+        vtoraya.show();
+        Error tretiya = new Error(false, 15, "Логическая ошибка");
+        tretiya.show();
+    }
+}
