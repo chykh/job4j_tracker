8acf59c679f9481d71f1775b007fa20f9243ff82
chykh <chykh@mail.ru>
2022-01-14 13:32:27 +0300

    вспомогательный

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+        Calculator calculator = new Calculator();
+        int rsl = calculator.multiply(10);
+        System.out.println(rsl);
+    }
+}
