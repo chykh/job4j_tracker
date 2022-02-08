1c44028d323e71d127b737d9d938aa76983f077d
chykh <chykh@mail.ru>
2022-02-08 14:01:58 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,11 @@
 package ru.job4j.tracker;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class StartUI {
 
     public static void main(String[] args) {
-        Item test = new Item();
-        LocalDateTime lcd = test.getLocalDateTime();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String lcdf = lcd.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + lcdf);
+        Item test = new Item(14, "заявка");
+        System.out.println(test);
 
+        System.out.println(test.getLocalDateTime());
     }
 }
