0b9d004d9db92b7d888a6d302275b086e5017629
chykh <chykh@mail.ru>
2022-02-10 20:05:06 +0300

    Вспомогательный

--- src/main/java/ru/job4j/oop/Transport.java
+++ src/main/java/ru/job4j/oop/Transport.java
@@ -1,4 +1,15 @@
 package ru.job4j.oop;
 
 public class Transport {
+    public static void main(String[] args) {
+        Vehicle plane = new Plane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+        Vehicle[] vehicle = new Vehicle[]{plane, train, bus};
+        for (Vehicle vec : vehicle) {
+            vec.move();
+            vec.repare();
+        }
+    }
+
 }
