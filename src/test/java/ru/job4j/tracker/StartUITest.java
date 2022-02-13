434c2df954e4c0fa00a13b7df2ee7d553d055ad0
chykh <chykh@mail.ru>
2022-02-13 18:37:33 +0300

    9. Написать тесты на StartUI. [#785]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+public class StartUITest {
+
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(new String[] {"0", "Item name", "1"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new CreateAction(), new Exit()};
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        String replacedName = "New item name";
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced item"));
+        Input in = new StubInput(new String[] {"0", "1", replacedName, "1"});
+        UserAction[] actions = {new ReplaceAction(), new Exit()};
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item"));
+        Input in = new StubInput(new String[] {"0", "1", "1"});
+        UserAction[] actions = {new DeleteAction(), new Exit() };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+}
