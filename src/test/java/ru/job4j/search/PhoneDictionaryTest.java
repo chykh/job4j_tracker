35b8ee1dbbd4eea7ea7a504313d6182e7aa54986
chykh <chykh@mail.ru>
2022-03-11 18:10:25 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -25,6 +25,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Sasha");
-        assertEquals(persons, new ArrayList<>());
+        assertTrue(persons.isEmpty());
     }
 }
