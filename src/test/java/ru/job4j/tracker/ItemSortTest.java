0957aa3f8e43e837fb95ae740caaf33453cd6603
chykh <chykh@mail.ru>
2022-03-23 20:16:38 +0300

    0. Сортировка [#10096 #277810]

--- src/test/java/ru/job4j/tracker/ItemSortTest.java
+++ src/test/java/ru/job4j/tracker/ItemSortTest.java
@@ -1,13 +1,17 @@
 package ru.job4j.tracker;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ItemSortTest extends TestCase {
+import static org.junit.Assert.assertEquals;
 
+public class ItemSortTest {
+
+    @Test
     public void testCompare() {
             List<Item> items = Arrays.asList(new Item("C - first"),
                     new Item("A - second"), new Item("B - third"));
@@ -19,6 +23,7 @@ public class ItemSortTest extends TestCase {
 
     }
 
+    @Test
     public void testReverseCompare() {
         List<Item> items = Arrays.asList(new Item("C - first"),
                 new Item("A - second"), new Item("B - third"));
