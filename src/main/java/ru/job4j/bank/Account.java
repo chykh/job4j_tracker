210b0048e0f5850b2dfdb7d7478a996fca1989e5
chykh <chykh@mail.ru>
2022-03-30 20:49:21 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,8 +2,17 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель данных типа Account
+ */
 public class Account {
+    /**
+     * содержит реквизиты данного аккаунта
+     */
     private String requisite;
+    /**
+     * содержит баланс данного аккаунта
+     */
     private double balance;
 
     public Account(String requisite, double balance) {
@@ -11,22 +20,43 @@ public class Account {
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет получить реквизиты данного аккаунта
+     * @return Возвращает значение поля requisite
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод присваивает полю класса requisite значение из параметра
+     * @param requisite реквизиты, которые мы хотим установить
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод позволяет получить поля balance
+     * @return Возвращает значение поля balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод позволяет установить баланс для данного Аккаунта
+     * @param balance  значение из параметра balance, которое мы хотим установить
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет проверить равенство двух объектов
+     * @param o ссылка на объект, который мы хотим сравнить с текущим объектом
+     * @return Возвращает булево значение о равенстве объекта из параметра текущему объекту
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +69,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод позволяет вернуть хэшкод данного объекта
+     * @return Возвращает хэшкод
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает банковский сервис, который может быть оказан клиенту
+ */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод позволяет добавить нового пользователя в систему и создать для него
+     * список будущих аккаунтов
+     * @param user принимает в качестве параметра ссылку на объект типа User
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод позволяет добавить к данному пользователю новый аккаунт
+     * @param passport данные о паспорте пользователя
+     * @param account данные о паспорте пользователя
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -22,6 +35,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод позволяет определить пользователя по паспорту
+     * @param passport данные о паспорте пользователя
+     * @return Возвращает ссылку на объект User искомого пользователя
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
@@ -31,6 +49,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяет определить аккаунт пользователя по его паспорту и реквизитам
+     * @param passport данные о паспорте пользователя
+     * @param requisite данные о реквизитах пользователя
+     * @return Возвращает аккаунт пользователя, соответствующим его реквизитам
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -44,6 +68,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяет выполнить перевод денег между двумя пользователями,
+     * убедившись, что денежных средств на счету плательщика достаточно
+     * @param srcPassport данные о паспорте плательщика перевода
+     * @param srcRequisite данные о реквизитах плательщика перевода
+     * @param destPassport данные о паспорте получателя перевода
+     * @param destRequisite данные о реквизитах получателя перевода
+     * @param amount данные о сумме перевода
+     * @return Возвращает булево значение о результате перевода (true - положительный исход)
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite,
                                  double amount) {
