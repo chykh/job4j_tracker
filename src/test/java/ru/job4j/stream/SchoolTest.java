270f69bcf9ef857b8ff02db1c3ed600c89336093
chykh <chykh@mail.ru>
2022-04-22 16:39:31 +0300

    1. Фильтрация учеников [#110058]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -38,7 +38,7 @@ public class SchoolTest {
                 new Student(80, "Surname8")
         );
         School sc = new School();
-        Predicate<Student> pr = student -> student.getScore()<70 && student.getScore() >= 50;
+        Predicate<Student> pr = student -> student.getScore() < 70 && student.getScore() >= 50;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));
