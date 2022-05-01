6856b8308de43a7dfbbf3246f9669fa09335d507
chykh <chykh@mail.ru>
2022-05-01 15:14:16 +0300

    3. Optional в банковских переводах. [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,42 +1,40 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
     private final Map<Student, Set<Subject>> students;
-    
+
     public College(Map<Student, Set<Subject>> students) {
         this.students = students;
     }
-    
-    public Student findByAccount(String account) {
-        return students.keySet().stream()
-                .filter(s -> s.getAccount().equals(account))
-                .findFirst().orElse(null);
-    }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
-            Set<Subject> subjects = students.get(a);
-            return subjects.stream().filter(s -> s.getName().equals(name))
-                    .findFirst().orElse(null);
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s: students.keySet()) {
+            if (account.equals(s.getAccount())) {
+                rsl = Optional.of(s);
+                break;
+            }
         }
-        return null;
+        return rsl;
     }
 
-    public static void main(String[] args) {
-        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
-                Set.of(
-                        new Subject("Math", 70),
-                        new Subject("English", 85)
-                )
-        );
-        College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.getScore());
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> rsl = Optional.empty();
+        if (findByAccount(account).isEmpty()) {
+            return Optional.empty();
+        }
+
+        Student s = findByAccount(account).get();
+            Set<Subject> subjects = students.get(s);
+            for (Subject subj : subjects) {
+                if (name.equals(subj.getName())) {
+                    return Optional.of(subj);
+                }
+            }
+        return rsl;
     }
 }
