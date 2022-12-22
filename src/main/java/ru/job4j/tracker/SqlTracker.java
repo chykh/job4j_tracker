9f8d94ee479f8a6480e64c147bf77095086ecd60
chykh <chykh@mail.ru>
2022-12-22 20:08:33 +0300

    2. Трекер SQL [#1734]

---
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -0,0 +1,147 @@
+package ru.job4j.tracker;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store {
+
+    private Connection cn;
+
+    public SqlTracker() {
+        init();
+    }
+
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
+
+    private void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        try (PreparedStatement ps = cn.prepareStatement(
+                "insert into items(name, created) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, item.getName());
+            ps.setTimestamp(2, Timestamp.valueOf(item.getLocalDateTime()));
+            ps.execute();
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        try (PreparedStatement ps = cn.prepareStatement(
+                "update items set id = ?, name = ?, created = ? where id = ?")) {
+            ps.setInt(1, item.getId());
+            ps.setString(2, item.getName());
+            ps.setTimestamp(3, Timestamp.valueOf(item.getLocalDateTime()));
+            ps.setInt(4, id);
+            result = ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement ps = cn.prepareStatement(
+                "delete from items where id = ?")) {
+            ps.setInt(1, id);
+            result = ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement ps = cn.prepareStatement(
+            "select * from items")) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(new Item(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name")
+                            ));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement ps = cn.prepareStatement(
+                "select * from items where name = " + key)) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(new Item(
+                            resultSet.getInt("id"),
+                            key
+                    ));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Item item = null;
+        try (PreparedStatement ps = cn.prepareStatement(
+                "select * from items where id = ?")) {
+            ps.setInt(1, id);
+                try (ResultSet resultSet = ps.executeQuery()) {
+                    item =  new Item(
+                            id,
+                            resultSet.getString("name")
+                    );
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+}
